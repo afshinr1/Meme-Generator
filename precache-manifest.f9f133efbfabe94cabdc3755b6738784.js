b8fc9056698ece6096984a7324513cde6f0d646e
Afshin <afshinrahman0@gmail.com>
2020-05-31 14:18:04 -0600

    Updates

---
+++ precache-manifest.f9f133efbfabe94cabdc3755b6738784.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2da1a0570d04d2802feb98ecaae606ba",
+    "url": "/Meme-Generator/index.html"
+  },
+  {
+    "revision": "816b75c06ea2fd0f897a",
+    "url": "/Meme-Generator/static/css/main.e1d4bed9.chunk.css"
+  },
+  {
+    "revision": "79ababcd3d87033d4a63",
+    "url": "/Meme-Generator/static/js/2.5da1b30a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Meme-Generator/static/js/2.5da1b30a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "816b75c06ea2fd0f897a",
+    "url": "/Meme-Generator/static/js/main.f2c85f55.chunk.js"
+  },
+  {
+    "revision": "0f74f528d1951ec82b39",
+    "url": "/Meme-Generator/static/js/runtime-main.7cb5aa08.js"
+  },
+  {
+    "revision": "d55af749b9da4725e967495b4c02ef7c",
+    "url": "/Meme-Generator/static/media/trollface.d55af749.png"
+  }
+]);
